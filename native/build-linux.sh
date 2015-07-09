e3e095b13d948ac7460bc59463c3f5fbf9410b4a
Robert Dennington <tinytimrob@ginever.net>
2015-07-09 16:48:28 +0100

    Add freetype 2.6 library files

--- native/build-linux.sh
+++ native/build-linux.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-g++ -I /usr/lib/jvm/java-7-openjdk-amd64/include/ -I inc/ -L lib/linux32 -shared -lfmod -o libfreetype2jni.32.so src/*.cpp -fPIC -m32
-g++ -I /usr/lib/jvm/java-7-openjdk-amd64/include/ -I inc/ -L lib/linux64 -shared -lfmod -o libfreetype2jni.64.so src/*.cpp -fPIC -m64
+g++ -I /usr/lib/jvm/java-7-openjdk-amd64/include/ -I freetype-2.6/include/ -L lib/linux32 -shared -lfmod -o libfreetype2jni.32.so src/*.cpp -fPIC -m32
+g++ -I /usr/lib/jvm/java-7-openjdk-amd64/include/ -I freetype-2.6/include/ -L lib/linux64 -shared -lfmod -o libfreetype2jni.64.so src/*.cpp -fPIC -m64
 
