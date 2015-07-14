a1705c91b2827a9e1f5a69ea61359aa896ec776b
Robert Dennington <tinytimrob@ginever.net>
2015-07-14 13:11:47 +0100

    First build attempt for linux

--- native/build-linux.sh
+++ native/build-linux.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
-g++ -I /usr/lib/jvm/java-7-openjdk-amd64/include/ -I freetype-2.6/include/ -L lib/linux32 -shared -lfreetype26 -o libfreetype2jni.32.so src/*.cpp -fPIC -m32
-g++ -I /usr/lib/jvm/java-7-openjdk-amd64/include/ -I freetype-2.6/include/ -L lib/linux64 -shared -lfreetype26 -o libfreetype2jni.64.so src/*.cpp -fPIC -m64
+mkdir prebuilt prebuilt/linux32 prebuilt/linux64
+g++ -I /usr/lib/jvm/java-7-openjdk-amd64/include/ -I freetype-2.6/include/ -L lib/linux32 -shared -lfreetype -o prebuilt/linux32/libfreetype2jni.so src/*.cpp -fPIC -m32
+g++ -I /usr/lib/jvm/java-7-openjdk-amd64/include/ -I freetype-2.6/include/ -L lib/linux64 -shared -lfreetype -o prebuilt/linux64/libfreetype2jni.so src/*.cpp -fPIC -m64
 
